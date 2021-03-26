d1eda87962a636494c2e8c894e6d01631f7e53cd
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-25 22:04:20 -0400

    queue finished

--- chapter03/utils/Queue.js
+++ chapter03/utils/Queue.js
@@ -4,9 +4,28 @@ class Queue {
     this.back = null;
   }
 
-  add(value) {}
+  add(value) {
+    const node = new LinkedList(value);
+    if (this.front === null) {
+      this.front = node;
+      this.back = node;
+    } else {
+      const prevBack = this.back;
+      this.back = node;
+      prevBack.next = this.back;
+    }
+  }
 
-  remove() {}
+  remove() {
+    const removed = this.front;
+    if (this.front === this.back) {
+      this.front = null;
+      this.back = null;
+    } else {
+      this.front = this.front.next;
+    }
+    return removed !== null ? removed.value : null;
+  }
 
   peek() {
     return this.front !== null ? this.front.value : null;
