6db33b36e09f5234ee235f946574c7f5b73a00f8
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-10 21:50:03 -0400

    added binary search tree

---
+++ chapter04/utils/BinarySearchTree.js
@@ -0,0 +1,52 @@
+const Queue = require('./Queue');
+
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  printLevelOrder() {
+    const level = [];
+    const q = new Queue();
+    const nextq = new Queue();
+    let currNode;
+
+    q.add(this);
+    while (!q.isEmpty()) {
+      currNode = q.remove();
+      level.push(currNode.value);
+      if (currNode.left !== null) {
+        nextq.add(currNode.left);
+      }
+      if (currNode.right !== null) {
+        nextq.add(currNode.right);
+      }
+      if (q.isEmpty()) {
+        console.log(level.join(','));
+        level = [];
+        q = nextq;
+        nextq = new Queue();
+      }
+    }
+  }
+}
+
+module.exports = BinarySearchTree;
