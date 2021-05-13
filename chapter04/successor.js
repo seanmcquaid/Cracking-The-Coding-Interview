d48d8d828f527c77409f8c09ec4c509df968d887
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-12 22:18:15 -0400

    added successor solution

--- chapter04/successor.js
+++ chapter04/successor.js
@@ -1,1 +1,36 @@
-const successor = () => {};
+class BinarySearchTreeWithParent {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+const successor = (node) => {
+  if (!node) {
+    return;
+  }
+
+  if (node.right) {
+    node = node.right;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.value;
+  }
+
+  if (node.parent) {
+    if (node.parent.value > node.value) {
+      return node.parent.value;
+    } else {
+      let parent = node.parent;
+      while (parent) {
+        if (parent.value > node.value) {
+          return parent.value;
+        }
+        parent = parent.parent;
+      }
+    }
+  }
+};
