0bbb00ae9d0abf5da9a062b957ba809b545a93d3
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-14 19:43:17 -0500

    added bst for first common ancestor

---
+++ chapter04/firstCommonAncestor.js
@@ -0,0 +1,24 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+
+  isAncestor(node2) {
+    if (this === node2) {
+      return true;
+    } else {
+      let answer1 = false;
+      let answer2 = false;
+      if (this.left !== null) {
+        answer1 = this.left.isAncestor(node2);
+      }
+      if (this.right !== null) {
+        answer2 = this.right.isAncestor(node2);
+      }
+      return false || answer1 || answer2;
+    }
+  }
+}
